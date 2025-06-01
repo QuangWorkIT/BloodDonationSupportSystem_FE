98bbae139dbc684bb6456c4774c4335801077489
Nguyen Minh Quang <nguyenminhquangnmqlamdong@gmail.com>
2025-06-01 19:50:30 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+function App() {
+
+  return (
+    <>
+      <h1 className="text-blue-500">Hello world</h1>
+      <h2>nice </h2>
+      <Button variant="destructive" >Click</Button>
+    </>
+  )
+}
+
+export default App
