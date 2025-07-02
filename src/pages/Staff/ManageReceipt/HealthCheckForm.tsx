14eb585cb71be4844ca4f926609ba5af7f46aecb
Nguyen Minh Quang <nguyenminhquangnmqlamdong@gmail.com>
2025-07-02 16:24:22 +0700

    refactor(form): add more fields at health checkout form

--- src/pages/Staff/ManageReceipt/HealthCheckForm.tsx
+++ src/pages/Staff/ManageReceipt/HealthCheckForm.tsx
@@ -3,37 +3,42 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { X } from "lucide-react";
-
+import type { DonorCardProps } from "@/pages/Staff/ManageReceipt/DonorCard"
 interface HealthCheckData {
   fullName: string;
   birthDate: string;
-  bloodType: string;
-  rhFactor: string;
   height: string;
   weight: string;
   bloodPressure: string;
   temperature: string;
+  hbvYes: boolean,
+  hbvNo: boolean,
+  hb: string,
   hasReceivedBloodYes: boolean;
   hasReceivedBloodNo: boolean;
   additionalNotes: string;
   staffId: string;
   processCompleted: boolean;
 }
-
+interface HealthCheckoutProps {
+  currentDonor: DonorCardProps | null
+  handleCancle: () => void
+}
 interface FormErrors {
   [key: string]: string;
 }
 
-export default function HealthCheckForm() {
+export default function HealthCheckForm({ currentDonor, handleCancle }: HealthCheckoutProps) {
   const [formData, setFormData] = useState<HealthCheckData>({
-    fullName: "",
+    fullName: currentDonor?.memberName || "",
     birthDate: "",
-    bloodType: "",
-    rhFactor: "",
     height: "",
     weight: "",
     bloodPressure: "",
     temperature: "",
+    hbvYes: false,
+    hbvNo: false,
+    hb: "",
     hasReceivedBloodYes: false,
     hasReceivedBloodNo: false,
     additionalNotes: "",
@@ -46,6 +51,14 @@ export default function HealthCheckForm() {
   const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { id, type, value, checked } = e.target;
 
+    if (id === 'hbvYes') {
+      setFormData({ ...formData, hbvYes: true, hbvNo: false })
+      return
+    }
+    if (id === 'hbvNo') {
+      setFormData({ ...formData, hbvYes: false, hbvNo: true })
+      return
+    }
     // Special handling for hasReceivedBlood checkboxes to keep them exclusive
     if (id === "hasReceivedBloodYes") {
       setFormData({
@@ -68,12 +81,18 @@ export default function HealthCheckForm() {
 
   const validateForm = (): FormErrors => {
     const newErrors: FormErrors = {};
-    
+
     // Full Name validation
     if (!formData.fullName.trim()) {
       newErrors.fullName = "Đây là trường thông tin bắt buộc.";
     } else if (formData.fullName.length > 100) {
       newErrors.fullName = "Tên không được vượt quá 100 ký tự.";
+    }
+
+    if (!formData.hb.trim()) {
+      newErrors.hb = "Đây là trường thông tin bắt buộc.";
+    } else if (Number(formData.hb) >= 100 || Number(formData.hb) <= 0) {
+      newErrors.hb = "Thông số không hợp lệ";
     }
 
     // Birth Date validation
@@ -82,7 +101,7 @@ export default function HealthCheckForm() {
     } else {
       const birthDate = new Date(formData.birthDate);
       const currentDate = new Date();
-      
+
       if (birthDate > currentDate) {
         newErrors.birthDate = "Ngày sinh không thể ở tương lai.";
       } else if (currentDate.getFullYear() - birthDate.getFullYear() > 60) {
@@ -149,9 +168,9 @@ export default function HealthCheckForm() {
       const temp = parseFloat(formData.temperature);
       if (isNaN(temp)) {
         newErrors.temperature = "Nhiệt độ phải là số.";
-      } else if (temp < 25) {
+      } else if (temp < 34) {
         newErrors.temperature = "Nhiệt độ quá thấp.";
-      } else if (temp > 35) {
+      } else if (temp > 38) {
         newErrors.temperature = "Nhiệt độ quá cao.";
       }
     }
@@ -159,6 +178,9 @@ export default function HealthCheckForm() {
     // Blood donation history validation
     if (!formData.hasReceivedBloodYes && !formData.hasReceivedBloodNo) {
       newErrors.hasReceivedBloodYes = "Vui lòng chọn một trong hai.";
+    }
+    if (!formData.hbvYes && !formData.hbvNo) {
+      newErrors.hbvYes = "Vui lòng chọn một trong hai";
     }
 
     // Process completion validation
@@ -186,7 +208,9 @@ export default function HealthCheckForm() {
       {/* Header */}
       <div className="flex justify-between items-center">
         <h1 className="text-2xl font-normal text-black">Đơn đánh giá tình trạng bệnh</h1>
-        <button className="text-gray-500 hover:text-gray-700 cursor-pointer">
+        <button
+          onClick={handleCancle}
+          className="text-gray-500 hover:text-gray-700 cursor-pointer">
           <X size={20} />
         </button>
       </div>
@@ -295,6 +319,63 @@ export default function HealthCheckForm() {
               onChange={handleChange}
             />
             {errors.temperature && <p className="text-red-500 text-sm mt-1">{errors.temperature}</p>}
+          </div>
+        </div>
+
+        <div className="flex items-center gap-6 mb-[40px]">
+          <Label className="text-base w-1/4">
+            Viêm gan B<span className="text-red-500">*</span>
+          </Label>
+          <div className="flex flex-col gap-1 flex-1 ">
+            <div className="flex gap-10">
+              <div className="flex items-center gap-2">
+                <input
+                  type="radio"
+                  id="hbvYes"
+                  value="true"
+                  checked={formData.hbvYes}
+                  onChange={handleChange}
+                  className="w-5 h-5"
+                  required
+                />
+                <Label htmlFor="hasReceivedBloodYes" className="text-base">
+                  Có
+                </Label>
+              </div>
+              <div className="flex items-center gap-2">
+                <input
+                  type="radio"
+                  id="hbvNo"
+                  value="false"
+                  checked={formData.hbvNo}
+                  onChange={handleChange}
+                  className="w-5 h-5"
+                  required
+                />
+                <Label htmlFor="hasReceivedBloodNo" className="text-base">
+                  Không
+                </Label>
+              </div>
+            </div>
+            {errors.hbvYes && (
+              <p className="text-red-500 text-sm mt-1 w-full">{errors.hbvYes}</p>
+            )}
+          </div>
+        </div>
+
+        <div className="flex items-center gap-4 mb-[40px]">
+          <Label htmlFor="hb" className="text-base w-1/4">
+            Hàm lượng hemoglobin (g/dL)
+          </Label>
+          <div className="flex-1">
+            <Input
+              id="hb"
+              placeholder="VD: 13.5, 18.0,..."
+              className="py-4 text-base bg-[#F4F5F8] border-none"
+              value={formData.hb}
+              onChange={handleChange}
+            />
+            {errors.hb && <p className="text-red-500 text-sm mt-1">{errors.hb}</p>}
           </div>
         </div>
 
