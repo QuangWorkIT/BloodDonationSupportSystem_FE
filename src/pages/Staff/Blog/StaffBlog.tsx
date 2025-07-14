cd03cb740f9732a6feae2ef0a0ba11d8fb714f50
Hải Phúc <haiphuchoang2675@gmail.com>
2025-07-14 07:53:47 +0700

    refactor(ui): add background color

--- src/pages/Staff/Blog/StaffBlog.tsx
+++ src/pages/Staff/Blog/StaffBlog.tsx
@@ -85,7 +85,7 @@ export default function StaffBlogs() {
       {loading ? (
         <LoadingSpinner />
       ) : (
-        <motion.div className="p-8 space-y-6" initial={{ opacity: 0, y: 30 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }}>
+        <motion.div className="p-8 space-y-6 bg-gray-100" initial={{ opacity: 0, y: 30 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }}>
           <h1 className="text-3xl font-semibold text-red-700">Quản lý Blog</h1>
 
           {/* Form */}
